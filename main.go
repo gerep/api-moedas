fc849f596c9be313cb69da384c4b6ec271a953cb
alvarossantos <alvaroguimaraessantos@gmail.com>
2025-11-12 11:46:30 -0300

    Integrating Exchange and updating other

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"strings"
-	"bufio"
 )
 
 func loadEnv() {
@@ -15,7 +16,7 @@ func loadEnv() {
 		return
 	}
 	defer file.Close()
-	
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -34,16 +35,49 @@ func convertHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Fprint(w, "Currency API!")
+	apiKey := os.Getenv("API_KEY_EXCHANGE")
+	if apiKey == "" {
+		http.Error(w, "API key not configured", http.StatusInternalServerError)
+		return
+	}
+
+	url := fmt.Sprintf("http://api.exchangeratesapi.io/v1/latest?access_key=%s", apiKey)
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error making API request:", err)
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		fmt.Println("API request failed with status:", res.Status)
+		return
+	}
+	fmt.Println("API response status:", res.Status)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(res.StatusCode)
+
+	_, err = io.Copy(w, res.Body)
+	if err != nil {
+		fmt.Println("Error copying response body:", err)
+	}
 }
 
 func main() {
 	loadEnv()
+
+	apiKey := os.Getenv("API_KEY_EXCHANGE")
+	if apiKey == "" {
+		fmt.Println("API_KEY_EXCHANGE environment variable not set")
+		return
+	}
 	http.HandleFunc("/convert", convertHandler)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+	fmt.Printf("Server is running on port %s...\n", port)
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
